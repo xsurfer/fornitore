4866f49ffb41936f8bbcd87baecf2b87eae8c344
fabio <fabio@linuxbox>
2013-01-12 17:24:01 +0100

    Commit from JBoss Tools

--- src/main/java/org/jboss/as/quickstarts/wshelloworld/FornitoreService.java
+++ src/main/java/org/jboss/as/quickstarts/wshelloworld/FornitoreService.java
@@ -29,35 +29,37 @@ import javax.jws.WebService;
  */
 
 @WebService(targetNamespace = "http://fornitore-paasfab.rhcloud.com/jboss-as-helloworld-ws/HelloWorldService")
-public interface HelloWorldService {
+public interface FornitoreService {
 
+	@WebMethod
+    public Event getEvent(Integer idEvent);
+	
 	@WebMethod
     public ArrayList<Event> getEvents();
 	
-	
-    /**
-     * Say hello as a response
-     * 
-     * @return A simple hello world message
-     */
-    @WebMethod
-    public String sayHello();
-
-    /**
-     * Say hello to someone precisely
-     * 
-     * @param name The name of the person to say hello to
-     * @return the number of current bookings
-     */
-    @WebMethod
-    public String sayHelloToName(String name);
-
-    /**
-     * Say hello to a list of people
-     * 
-     * @param names The list of names to say hello to
-     * @return the number of current bookings
-     */
-    @WebMethod
-    public String sayHelloToNames(List<String> names);
+//    /**
+//     * Say hello as a response
+//     * 
+//     * @return A simple hello world message
+//     */
+//    @WebMethod
+//    public String sayHello();
+//
+//    /**
+//     * Say hello to someone precisely
+//     * 
+//     * @param name The name of the person to say hello to
+//     * @return the number of current bookings
+//     */
+//    @WebMethod
+//    public String sayHelloToName(String name);
+//
+//    /**
+//     * Say hello to a list of people
+//     * 
+//     * @param names The list of names to say hello to
+//     * @return the number of current bookings
+//     */
+//    @WebMethod
+//    public String sayHelloToNames(List<String> names);
 }
