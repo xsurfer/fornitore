5ea006d46f561a748079bb6189501f2e0588bab9
Fabio Perfetti <perfabio87@gmail.com>
2013-02-10 01:57:06 +0000

    Commit from JBoss Tools

--- src/main/java/org/jboss/as/quickstarts/wshelloworld/FornitoreServiceImpl.java
+++ src/main/java/org/jboss/as/quickstarts/wshelloworld/FornitoreServiceImpl.java
@@ -21,9 +21,6 @@ import java.util.ArrayList;
 import javax.jws.WebService;
 
 import org.hibernate.*;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 import org.jboss.as.quickstarts.wshelloworld.model.*;
 import org.jboss.as.quickstarts.wshelloworld.util.HibernateUtil;
 
